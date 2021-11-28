9e497163e8b1d0821f14022d429b900c9856da83
Piotr <p.skolorzynski@gmail.com>
2021-11-28 01:07:03 +0100

    modify package.json install @fortawesome packages to use fontawesome icons modify CustomizedTable component Modify table structure and styling Add data to table modify table.css file add styling

--- src/components/Dashboard/table/CustomizedTable.js
+++ src/components/Dashboard/table/CustomizedTable.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 import "./table.css";
+import PrintIcon from "@mui/icons-material/Print";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDumbbell,
+  faCheck,
+  faCheckCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import TagFacesIcon from "@mui/icons-material/TagFaces";
 
 const TitleHours = styled.td`
   height: 50px;
@@ -7,22 +16,11 @@ const TitleHours = styled.td`
 `;
 
 const TitleDay = styled.td`
+  width: 149px;
   background-color: #fff;
   text-align: center;
   text-transform: uppercase;
-  color: var(--seconadary-text-color);
   font-size: 24px;
-  border-left: 2px solid var(--secondary-background);
-  border-bottom: 2px solid var(--secondary-background);
-`;
-
-const TitleDaySide = styled.td`
-  background-color: #fff;
-  text-align: center;
-  text-transform: uppercase;
-  color: var(--seconadary-text-color);
-  font-size: 24px;
-  border-bottom: 2px solid var(--secondary-background);
 `;
 
 const Hours = styled.td`
@@ -40,22 +38,9 @@ const HoursFooter = styled.td`
 `;
 
 const DayCell = styled.td`
+  padding: 10px;
   background-color: #fff;
-  text-align: center;
-  text-transform: uppercase;
-  color: var(--seconadary-text-color);
-  font-size: 24px;
-  border-left: 2px solid var(--secondary-background);
-  border-bottom: 2px solid var(--secondary-background);
-`;
-
-const DayCellSide = styled.td`
-  background-color: #fff;
-  text-align: center;
-  text-transform: uppercase;
-  color: var(--seconadary-text-color);
-  font-size: 24px;
-  border-bottom: 2px solid var(--secondary-background);
+  font-size: 14px;
 `;
 
 const DayCellFooter = styled.td`
@@ -63,21 +48,14 @@ const DayCellFooter = styled.td`
   font-weight: 500;
   text-align: center;
   text-transform: uppercase;
-  border-bottom: 2px solid #c7c6c6;
-  border-left: 2px solid #c7c6c6;
-`;
-const DayCellFooterSide = styled.td`
-  font-size: 12px;
-  font-weight: 500;
-  text-align: center;
-  text-transform: uppercase;
-  border-bottom: 2px solid #c7c6c6;
 `;
 
 const WorkoutTitle = styled.td`
   height: 35px;
-  font-size: 10px;
-  font-weight: 500;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
   text-align: center;
 `;
 
@@ -85,111 +63,199 @@ const WorkoutCell = styled.td`
   font-size: 10px;
   font-weight: 500;
   text-align: center;
-  border-left: 2px solid #c7c6c6;
-`;
-
-const WorkoutCellSide = styled.td`
-  font-size: 10px;
-  font-weight: 500;
-  text-align: center;
 `;
 
 const CustomizedTable = () => {
+  const changeActivityStatus = (e) => {
+    console.log(e.target.classList);
+  };
+
   return (
-    <table cellspacing="0" cellpadding="0" border="0" className="table">
-      <tr>
-        <TitleHours className="cell-background"></TitleHours>
-        <TitleDaySide>Day 64</TitleDaySide>
-        <TitleDay>Day 65</TitleDay>
-        <TitleDay>Day 66</TitleDay>
-        <TitleDay>Day 67</TitleDay>
-        <TitleDay>Day 68</TitleDay>
-        <TitleDay>Day 69</TitleDay>
-        <TitleDay>Day 70</TitleDay>
-      </tr>
-      <tr>
-        <Hours className="cell-background">
-          <b>6:00</b> AM
-        </Hours>
-        <DayCellSide>Day 64</DayCellSide>
-        <DayCell>Day 65</DayCell>
-        <DayCell>Day 66</DayCell>
-        <DayCell>Day 67</DayCell>
-        <DayCell>Day 68</DayCell>
-        <DayCell>Day 69</DayCell>
-        <DayCell>Day 70</DayCell>
-      </tr>
-      <tr>
-        <Hours className="cell-background">
-          <b>9:00</b> AM
-        </Hours>
-        <DayCellSide>Day 64</DayCellSide>
-        <DayCell>Day 65</DayCell>
-        <DayCell>Day 66</DayCell>
-        <DayCell>Day 67</DayCell>
-        <DayCell>Day 68</DayCell>
-        <DayCell>Day 69</DayCell>
-        <DayCell>Day 70</DayCell>
-      </tr>
-      <tr>
-        <Hours className="cell-background">
-          <b>12:00</b> AM
-        </Hours>
-        <DayCellSide>Day 64</DayCellSide>
-        <DayCell>Day 65</DayCell>
-        <DayCell>Day 66</DayCell>
-        <DayCell>Day 67</DayCell>
-        <DayCell>Day 68</DayCell>
-        <DayCell>Day 69</DayCell>
-        <DayCell>Day 70</DayCell>
-      </tr>
-      <tr>
-        <Hours className="cell-background">
-          <b>3:00</b> PM
-        </Hours>
-        <DayCellSide>Day 64</DayCellSide>
-        <DayCell>Day 65</DayCell>
-        <DayCell>Day 66</DayCell>
-        <DayCell>Day 67</DayCell>
-        <DayCell>Day 68</DayCell>
-        <DayCell>Day 69</DayCell>
-        <DayCell>Day 70</DayCell>
-      </tr>
-      <tr>
-        <Hours className="cell-background">
-          <b>6:00</b> PM
-        </Hours>
-        <DayCellSide className="daycell-bottom">Day 64</DayCellSide>
-        <DayCell className="daycell-bottom">Day 65</DayCell>
-        <DayCell className="daycell-bottom">Day 66</DayCell>
-        <DayCell className="daycell-bottom">Day 67</DayCell>
-        <DayCell className="daycell-bottom">Day 68</DayCell>
-        <DayCell className="daycell-bottom">Day 69</DayCell>
-        <DayCell className="daycell-bottom">Day 70</DayCell>
-      </tr>
-      <tr>
-        <HoursFooter className="cell-background"></HoursFooter>
-        <DayCellFooterSide className="cell-background">
-          low-carb
-        </DayCellFooterSide>
-        <DayCellFooter className="cell-background">low-carb</DayCellFooter>
-        <DayCellFooter className="cell-background">high-carb</DayCellFooter>
-        <DayCellFooter className="cell-background">low-carb</DayCellFooter>
-        <DayCellFooter className="cell-background">low-carb</DayCellFooter>
-        <DayCellFooter className="cell-background">low-carb</DayCellFooter>
-        <td rowspan="2" className="print-cell">
-          Print
-        </td>
-      </tr>
-      <tr>
-        <WorkoutTitle className="cell-background">Workout</WorkoutTitle>
-        <WorkoutCellSide className="cell-background">Day 64</WorkoutCellSide>
-        <WorkoutCell className="cell-background">Day 65</WorkoutCell>
-        <WorkoutCell className="cell-background">Day 66</WorkoutCell>
-        <WorkoutCell className="cell-background">Day 67</WorkoutCell>
-        <WorkoutCell className="cell-background">Day 68</WorkoutCell>
-        <WorkoutCell className="cell-background">Day 69</WorkoutCell>
-      </tr>
+    <table cellSpacing="0" cellPadding="0" border="0" className="table">
+      <tbody>
+        <tr>
+          <TitleHours className="cell-background"></TitleHours>
+          <TitleDay className="title-day--side">Day 64</TitleDay>
+          <TitleDay className="title-day title-day--active">Day 65</TitleDay>
+          <TitleDay className="title-day">Day 66</TitleDay>
+          <TitleDay className="title-day">Day 67</TitleDay>
+          <TitleDay className="title-day">Day 68</TitleDay>
+          <TitleDay className="title-day">Day 69</TitleDay>
+          <TitleDay className="title-day">Day 70</TitleDay>
+        </tr>
+        <tr>
+          <Hours className="cell-background">
+            <b>6:00</b> AM
+          </Hours>
+          <DayCell className="daycell--side">
+            Bode*e Shake
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              className="check-icon-cirlce check-icon-cirlce--show"
+            />
+          </DayCell>
+          <DayCell className="daycell daycell--active">Bode*e Shake</DayCell>
+          <DayCell className="daycell">Bode*e Shake</DayCell>
+          <DayCell className="daycell">Bode*e Shake</DayCell>
+          <DayCell className="daycell">Bode*e Shake</DayCell>
+          <DayCell className="daycell">Bode*e Shake</DayCell>
+          <td rowSpan="5" className="freedaycell">
+            guilt-free day <TagFacesIcon fontSize="large" sx={{ mb: 2 }} />
+          </td>
+        </tr>
+        <tr>
+          <Hours className="cell-background">
+            <b>9:00</b> AM
+          </Hours>
+          <DayCell className="daycell--side">
+            Ham and Swiss Roll Ups{" "}
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              className="check-icon-cirlce check-icon-cirlce--show"
+            />
+          </DayCell>
+          <DayCell className="daycell daycell--active">
+            Ham and Swiss Roll Ups
+          </DayCell>
+          <DayCell className="daycell">Grilled Steak (HC)</DayCell>
+          <DayCell className="daycell">Ham and Swiss Roll Ups</DayCell>
+          <DayCell className="daycell">Ham and Swiss Roll Ups</DayCell>
+          <DayCell className="daycell">Grilled Steak (HC)</DayCell>
+        </tr>
+        <tr>
+          <Hours className="cell-background">
+            <b>12:00</b> AM
+          </Hours>
+          <DayCell className="daycell--side">
+            Turkey Melt
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              className="check-icon-cirlce check-icon-cirlce--show"
+            />
+          </DayCell>
+          <DayCell
+            className="daycell daycell--active"
+            onClick={changeActivityStatus}
+          >
+            Turkey Melt
+          </DayCell>
+          <DayCell className="daycell">Bode*e Shake</DayCell>
+          <DayCell className="daycell">Turkey Melt</DayCell>
+          <DayCell className="daycell">Bode*e Shake</DayCell>
+          <DayCell className="daycell">Bode*e Shake</DayCell>
+        </tr>
+        <tr>
+          <Hours className="cell-background">
+            <b>3:00</b> PM
+          </Hours>
+          <DayCell className="daycell--side">
+            Bode*e Burn with Mozarella and Tomato Slices{" "}
+            <FontAwesomeIcon
+              icon={faCheckCircle}
+              className="check-icon-cirlce check-icon-cirlce--show"
+            />
+          </DayCell>
+          <DayCell className="daycell daycell--active">
+            Bode*e Burn with Mozarella and Tomato Slices
+          </DayCell>
+          <DayCell className="daycell">
+            Bode*e Burn and Whole-Wheat English Muffin with Butter Spray
+          </DayCell>
+          <DayCell className="daycell">
+            Bode*e Burn with Mozarella and Tomato Slices
+          </DayCell>
+          <DayCell className="daycell">
+            Bode*e Burn with Mozarella and Tomato Slices
+          </DayCell>
+          <DayCell className="daycell">
+            Bode*e Burn and Whole-Wheat English Muffin with Butter Spray
+          </DayCell>
+        </tr>
+        <tr>
+          <Hours className="cell-background">
+            <b>6:00</b> PM
+          </Hours>
+          <DayCell className="daycell--side daycell--bottom">
+            Turkey Melt
+          </DayCell>
+          <DayCell className="daycell daycell--bottom daycell--active">
+            Turkey Melt
+          </DayCell>
+          <DayCell className="daycell daycell--bottom">
+            Garlic Lime Chicken (HC)
+          </DayCell>
+          <DayCell className="daycell daycell--bottom">Turkey Melt</DayCell>
+          <DayCell className="daycell daycell--bottom">Turkey Melt</DayCell>
+          <DayCell className="daycell daycell--bottom">
+            Garlic Lime Chicken (HC)
+          </DayCell>
+        </tr>
+        <tr>
+          <HoursFooter className="cell-background"></HoursFooter>
+          <DayCellFooter className="cell-background daycell-footer--side">
+            low-carb
+          </DayCellFooter>
+          <DayCellFooter className="cell-background daycell-footer daycell-footer--active">
+            low-carb
+          </DayCellFooter>
+          <DayCellFooter className="cell-background daycell-footer">
+            high-carb
+          </DayCellFooter>
+          <DayCellFooter className="cell-background daycell-footer">
+            low-carb
+          </DayCellFooter>
+          <DayCellFooter className="cell-background daycell-footer">
+            low-carb
+          </DayCellFooter>
+          <DayCellFooter className="cell-background daycell-footer">
+            low-carb
+          </DayCellFooter>
+          <td rowSpan="2" className="print-cell">
+            <PrintIcon fontSize="medium" /> <span>Print</span>
+          </td>
+        </tr>
+        <tr>
+          <WorkoutTitle className="cell-background">
+            <span>Workout</span> <ArrowRightIcon fontSize="small" />
+          </WorkoutTitle>
+          <WorkoutCell className="cell-background workoutcell--side">
+            <FontAwesomeIcon
+              icon={faDumbbell}
+              className="dumbbell-icon--checked"
+            />
+            <FontAwesomeIcon
+              icon={faCheck}
+              className="check-icon check-icon--show"
+            />
+          </WorkoutCell>
+          <WorkoutCell className="cell-background workoutcell workoutcell--active">
+            <FontAwesomeIcon
+              icon={faDumbbell}
+              className="dumbbell-icon--checked"
+            />
+            <FontAwesomeIcon
+              icon={faCheck}
+              className="check-icon check-icon--show"
+            />
+          </WorkoutCell>
+          <WorkoutCell className="cell-background workoutcell">
+            <FontAwesomeIcon icon={faDumbbell} className="dumbbell-icon" />
+            <FontAwesomeIcon icon={faCheck} className="check-icon" />
+          </WorkoutCell>
+          <WorkoutCell className="cell-background workoutcell">
+            <FontAwesomeIcon icon={faDumbbell} className="dumbbell-icon" />
+            <FontAwesomeIcon icon={faCheck} className="check-icon" />
+          </WorkoutCell>
+          <WorkoutCell className="cell-background workoutcell">
+            <FontAwesomeIcon icon={faDumbbell} className="dumbbell-icon" />
+            <FontAwesomeIcon icon={faCheck} className="check-icon" />
+          </WorkoutCell>
+          <WorkoutCell className="cell-background workoutcell">
+            <FontAwesomeIcon icon={faDumbbell} className="dumbbell-icon" />
+            <FontAwesomeIcon icon={faCheck} className="check-icon" />
+          </WorkoutCell>
+        </tr>
+      </tbody>
     </table>
   );
 };
